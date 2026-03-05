a5ef1a4ed224bb78c9e77bbb078fd5f7d8dd70a1
shaahidzx <shaahidzx@gmail.com>
2026-03-05 23:30:46 +0530

    centralized global exception handling for review service

--- review-services/com.gotrip.review_service.service.ReviewService.java
+++ review-services/com.gotrip.review_service.service.ReviewService.java
@@ -2,3 +2,4 @@ public Double getAverageRating(Long transportId) {
     return reviewRepository.findAllByTransportId(transportId)
             .stream().mapToInt(Review::getRating).average().orElse(0.0);
 }
+

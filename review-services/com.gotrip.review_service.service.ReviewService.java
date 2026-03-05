b67658983684ca5df4c2074cc662d2a5359bf5c2
shaahidzx <shaahidzx@gmail.com>
2026-03-05 23:03:04 +0530

    average rating calculation

---
+++ review-services/com.gotrip.review_service.service.ReviewService.java
@@ -0,0 +1,4 @@
+public Double getAverageRating(Long transportId) {
+    return reviewRepository.findAllByTransportId(transportId)
+            .stream().mapToInt(Review::getRating).average().orElse(0.0);
+}
